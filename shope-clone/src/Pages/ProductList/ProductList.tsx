1398ce4e5a7eea2ceef756c16ce81eafdc172ee2
Huynh Nhat Duy <huynhnhatduy1824@gmail.com>
2024-08-13 01:03:38 +0700

    xử lý thanh tìm kiếm

--- shope-clone/src/Pages/ProductList/ProductList.tsx
+++ shope-clone/src/Pages/ProductList/ProductList.tsx
@@ -1,73 +1,73 @@
-import { keepPreviousData, useQuery } from '@tanstack/react-query'
-import AsideFilter from './Components/AsideFilter'
-import Product from './Components/Product/Product'
-import SortProductList from './Components/SortProductList'
-import useQueryParams from 'src/hooks/useQueryParams'
-import productApi from 'src/apis/product.api'
-import Pagination from 'src/components/Pagination'
-import { ProductListConfig } from 'src/types/product.type'
-import { isUndefined, omitBy } from 'lodash'
-import CategoryApi from 'src/apis/category.api'
-
-export type QueryConfig = {
-  [key in keyof ProductListConfig]: string
-}
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import AsideFilter from "./Components/AsideFilter";
+import Product from "./Components/Product/Product";
+import SortProductList from "./Components/SortProductList";
+import productApi from "src/apis/product.api";
+import Pagination from "src/components/Pagination";
+import { ProductListConfig } from "src/types/product.type";
+import CategoryApi from "src/apis/category.api";
+import useQueryConfig from "src/hooks/useQueryConfig";
 
 export default function ProductList() {
-  const queryParams: QueryConfig = useQueryParams()
-  const queryConfig: QueryConfig = omitBy(
-    {
-      page: queryParams.page || '1',
-      limit: queryParams.limit || '20',
-      exclude: queryParams.exclude,
-      name: queryParams.name,
-      order: queryParams.order,
-      price_max: queryParams.price_max,
-      price_min: queryParams.price_min,
-      rating_filter: queryParams.rating_filter,
-      sort_by: queryParams.sort_by,
-      category: queryParams.category
-    },
-    isUndefined
-  )
+    const queryConfig = useQueryConfig();
 
-  const { data: ProductsData } = useQuery({
-    queryKey: ['/products', queryConfig],
-    queryFn: () => {
-      return productApi.getProducts(queryConfig as ProductListConfig)
-    },
-    placeholderData: keepPreviousData
-  })
+    const { data: ProductsData } = useQuery({
+        queryKey: ["/products", queryConfig],
+        queryFn: () => {
+            return productApi.getProducts(queryConfig as ProductListConfig);
+        },
+        placeholderData: keepPreviousData,
+    });
 
-  const { data: CategoriesData } = useQuery({
-    queryKey: ['/categories'],
-    queryFn: () => {
-      return CategoryApi.getCategories()
-    }
-  })
+    const { data: CategoriesData } = useQuery({
+        queryKey: ["/categories"],
+        queryFn: () => {
+            return CategoryApi.getCategories();
+        },
+    });
 
-  return (
-    <div className='bg-gray-200 py-6'>
-      <div className='container'>
-        {ProductsData && (
-          <div className='grid grid-cols-12 gap-6'>
-            <div className='col-span-3'>
-              {CategoriesData && <AsideFilter categories={CategoriesData.data.data || []} queryConfig={queryConfig} />}
+    return (
+        <div className="bg-gray-200 py-6">
+            <div className="container">
+                {ProductsData && (
+                    <div className="grid grid-cols-12 gap-6">
+                        <div className="col-span-3">
+                            {CategoriesData && (
+                                <AsideFilter
+                                    categories={CategoriesData.data.data || []}
+                                    queryConfig={queryConfig}
+                                />
+                            )}
+                        </div>
+                        <div className="col-span-9">
+                            <SortProductList
+                                queryConfig={queryConfig}
+                                pageSize={
+                                    ProductsData.data.data.pagination.page_size
+                                }
+                            />
+                            <div className="mt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3">
+                                {ProductsData.data.data.products.map(
+                                    (product) => (
+                                        <div
+                                            className="col-span-1"
+                                            key={product._id}
+                                        >
+                                            <Product product={product} />
+                                        </div>
+                                    )
+                                )}
+                            </div>
+                            <Pagination
+                                queryConfig={queryConfig}
+                                pageSize={
+                                    ProductsData.data.data.pagination.page_size
+                                }
+                            />
+                        </div>
+                    </div>
+                )}
             </div>
-            <div className='col-span-9'>
-              <SortProductList queryConfig={queryConfig} pageSize={ProductsData.data.data.pagination.page_size} />
-              <div className='mt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3'>
-                {ProductsData.data.data.products.map((product) => (
-                  <div className='col-span-1' key={product._id}>
-                    <Product product={product} />
-                  </div>
-                ))}
-              </div>
-              <Pagination queryConfig={queryConfig} pageSize={ProductsData.data.data.pagination.page_size} />
-            </div>
-          </div>
-        )}
-      </div>
-    </div>
-  )
+        </div>
+    );
 }
--- shope-clone/src/utils/rules.ts
+++ shope-clone/src/utils/rules.ts
@@ -1,97 +1,105 @@
-import { RegisterOptions, UseFormGetValues } from 'react-hook-form'
-import * as yup from 'yup'
+import { RegisterOptions, UseFormGetValues } from "react-hook-form";
+import * as yup from "yup";
 
-type Rules = { [key in 'email' | 'password' | 'confirm_password']?: RegisterOptions }
+type Rules = {
+    [key in "email" | "password" | "confirm_password"]?: RegisterOptions;
+};
 
-export const getRules = (getValues?: UseFormGetValues<any>): Rules => ({
-  email: {
-    required: {
-      value: true,
-      message: 'Bắt buộc điền email'
-    },
-    pattern: {
-      value: /^\S+@\S+\.\S+$/,
-      message: 'Email không đúng định dạng'
-    },
-    maxLength: {
-      value: 160,
-      message: 'Email không quá 160 ký tự'
-    },
-    minLength: {
-      value: 5,
-      message: 'Email không ít hơn 5 ký tự'
-    }
-  },
-  password: {
-    required: {
-      value: true,
-      message: 'Bắt buộc điền password'
-    },
-    maxLength: {
-      value: 160,
-      message: 'Password không quá 160 ký tự'
-    },
-    minLength: {
-      value: 6,
-      message: 'Password không ít hơn 6 ký tự'
-    }
-  },
-  confirm_password: {
-    required: {
-      value: true,
-      message: 'Bắt buộc điền lại password'
-    },
-    maxLength: {
-      value: 160,
-      message: 'Password không quá 160 ký tự'
-    },
-    minLength: {
-      value: 6,
-      message: 'Password không ít hơn 5 ký tự'
-    },
-    validate:
-      typeof getValues === 'function'
-        ? (value: any) => value === getValues('password') || 'Nhập lại mật khẩu không khớp'
-        : undefined
-  }
-})
+// export const getRules = (getValues?: UseFormGetValues<any>): Rules => ({
+//     email: {
+//         required: {
+//             value: true,
+//             message: "Bắt buộc điền email",
+//         },
+//         pattern: {
+//             value: /^\S+@\S+\.\S+$/,
+//             message: "Email không đúng định dạng",
+//         },
+//         maxLength: {
+//             value: 160,
+//             message: "Email không quá 160 ký tự",
+//         },
+//         minLength: {
+//             value: 5,
+//             message: "Email không ít hơn 5 ký tự",
+//         },
+//     },
+//     password: {
+//         required: {
+//             value: true,
+//             message: "Bắt buộc điền password",
+//         },
+//         maxLength: {
+//             value: 160,
+//             message: "Password không quá 160 ký tự",
+//         },
+//         minLength: {
+//             value: 6,
+//             message: "Password không ít hơn 6 ký tự",
+//         },
+//     },
+//     confirm_password: {
+//         required: {
+//             value: true,
+//             message: "Bắt buộc điền lại password",
+//         },
+//         maxLength: {
+//             value: 160,
+//             message: "Password không quá 160 ký tự",
+//         },
+//         minLength: {
+//             value: 6,
+//             message: "Password không ít hơn 5 ký tự",
+//         },
+//         validate:
+//             typeof getValues === "function"
+//                 ? (value: any) =>
+//                       value === getValues("password") ||
+//                       "Nhập lại mật khẩu không khớp"
+//                 : undefined,
+//     },
+// });
 
 function textPriceMinMax(this: yup.TestContext<yup.AnyObject>) {
-  const { price_min, price_max } = this.parent as { price_min: string; price_max: string }
-  if (price_min !== '' && price_max !== '') {
-    return Number(price_max) >= Number(price_min)
-  }
-  return price_min !== '' || price_max !== ''
+    const { price_min, price_max } = this.parent as {
+        price_min: string;
+        price_max: string;
+    };
+    if (price_min !== "" && price_max !== "") {
+        return Number(price_max) >= Number(price_min);
+    }
+    return price_min !== "" || price_max !== "";
 }
 
 export const schema = yup.object({
-  email: yup
-    .string()
-    .required('Bắt buộc điền email')
-    .email('Email không đúng định dạng')
-    .max(160, 'Email không quá 160 ký tự')
-    .min(5, 'Email không ít hơn 5 ký tự'),
-  password: yup
-    .string()
-    .required('Bắt buộc nhập password')
-    .max(160, 'Password không quá 160 ký tự')
-    .min(6, 'Password không ít hơn 5 ký tự'),
-  confirm_password: yup
-    .string()
-    .required('Bắt buộc nhập lại password')
-    .max(160, 'Password không quá 160 ký tự')
-    .min(6, 'Password không ít hơn 5 ký tự')
-    .oneOf([yup.ref('password')], 'Nhập lại password không khớp'),
-  price_min: yup.string().test({
-    name: 'price-not-allow',
-    message: 'Giá không phù hợp',
-    test: textPriceMinMax
-  }),
-  price_max: yup.string().test({
-    name: 'price-not-allow',
-    message: 'Giá không phù hợp',
-    test: textPriceMinMax
-  })
-})
+    email: yup
+        .string()
+        .required("Bắt buộc điền email")
+        .email("Email không đúng định dạng")
+        .max(160, "Email không quá 160 ký tự")
+        .min(5, "Email không ít hơn 5 ký tự"),
+    password: yup
+        .string()
+        .required("Bắt buộc nhập password")
+        .max(160, "Password không quá 160 ký tự")
+        .min(6, "Password không ít hơn 5 ký tự"),
+    confirm_password: yup
+        .string()
+        .required("Bắt buộc nhập lại password")
+        .max(160, "Password không quá 160 ký tự")
+        .min(6, "Password không ít hơn 5 ký tự")
+        .oneOf([yup.ref("password")], "Nhập lại password không khớp"),
+    price_min: yup.string().test({
+        name: "price-not-allow",
+        message: "Giá không phù hợp",
+        test: textPriceMinMax,
+    }),
+    price_max: yup.string().test({
+        name: "price-not-allow",
+        message: "Giá không phù hợp",
+        test: textPriceMinMax,
+    }),
+    name: yup.string().trim().required("Tên sản phẩm là bắt buộc"),
+});
 
-export type Schema = yup.InferType<typeof schema>
+export type Schema = yup.InferType<typeof schema>;
