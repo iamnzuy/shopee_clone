7f263fe3b554a711d66d1de0314898ea826b5fa9
Huynh Nhat Duy <huynhnhatduy1824@gmail.com>
2024-08-22 00:32:29 +0700

    khai báo api cho user

---
+++ shope-clone/src/apis/user.api.ts
@@ -0,0 +1,26 @@
+import { User } from 'src/types/user.type'
+import { SuccessResponse } from 'src/types/utils.type'
+import http from 'src/utils/http'
+
+interface BodyUpdateProfile extends Omit<User, '_id' | 'roles' | 'createAt' | 'updateAt' | 'email'> {
+  password: string
+  newPassword: string
+}
+
+const userApi = {
+  getProfile() {
+    return http.get<SuccessResponse<User>>('me')
+  },
+  updateProfile(body: BodyUpdateProfile) {
+    return http.put('user', body)
+  },
+  uploadAvatar(body: FormData) {
+    return http.post<SuccessResponse<string>>('user/upload-avatar', body, {
+      headers: {
+        'Content-Type': 'multimpart/form-data'
+      }
+    })
+  }
+}
+
+export default userApi
